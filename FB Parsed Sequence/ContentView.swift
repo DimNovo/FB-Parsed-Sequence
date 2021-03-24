1e33c0a66962674d02cbad6d57ccaad3983c36f0
Dmitry Novosyolov <i1710@icloud.com>
2021-03-24 08:48:54 +0200

    Initial Commit

---
+++ FB Parsed Sequence/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FB Parsed Sequence
+//
+//  Created by Dmitry Novosyolov on 24/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FB Parsed Sequence/FB_Parsed_SequenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FB_Parsed_SequenceApp.swift
+//  FB Parsed Sequence
+//
+//  Created by Dmitry Novosyolov on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FB_Parsed_SequenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
